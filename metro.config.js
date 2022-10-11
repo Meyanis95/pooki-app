f325397b8b193c9d04bae6831aca24d37a4c7fa6
Yanis Meziane <yanismeziane@Yaniss-MacBook-Pro.local>
2022-10-11 18:11:27 +0200

    fixed app icon

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,23 @@
-module.exports = {
-  resolver: {
-    extraNodeModules: require("expo-crypto-polyfills"),
-  },
-};
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+// extra config is needed to enable `react-native-svg-transformer`
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+    },
+    resolver: {
+      extraNodeModules: require("expo-crypto-polyfills"),
+    },
+  };
+})();
+
+// module.exports = {
+//   resolver: {
+//     extraNodeModules: require("expo-crypto-polyfills"),
+//   },
+// };
